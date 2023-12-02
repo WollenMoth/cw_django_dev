9edd1e119e6c20a3174a266aeabf6660e6618b20
André Díaz <andre290800@gmail.com>
2023-12-02 19:23:49 +0000

    fix: 🐛 'AnonymousUser' object is not iterable

--- survey/templatetags/question_filters.py
+++ survey/templatetags/question_filters.py
@@ -5,9 +5,13 @@ register = template.Library()
 
 @register.filter
 def user_likes(question, user):
+    if not user.is_authenticated:
+        return False
     return question.user_likes(user)
 
 
 @register.filter
 def user_dislikes(question, user):
+    if not user.is_authenticated:
+        return False
     return question.user_dislikes(user)
